bb02197ee9c578610409ca6df96dd9c880e0c726
John Mathena <bobtart41@gmail.com>
2025-10-13 15:20:56 -0400

    add tinaCMS and new post

---
+++ tina/config.ts
@@ -0,0 +1,73 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export const postSchema = [
+    {
+        type: "string",
+        name: "author",
+        label: "author",
+        required: true,
+    },
+    {
+        type: "string",
+        name: "date",
+        label: "date",
+        required: true,
+    },
+    {
+        type: "string",
+        name: "title",
+        label: "title",
+        required: true,
+        isTitle: true,
+    },
+    { 
+        type: "rich-text",
+        name: "body",
+        label: "body",
+        required: true,
+        isBody: true,
+    },
+];
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "static",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+          name: "the_strays_blog_posts",
+          label: "The Strays Blog Posts",
+          path: "content/the-strays/posts",
+          fields: postSchema,
+      },
+      {
+          name: "droppin_dimes_poems",
+          label: "Droppin Dimes Poems",
+          path: "content/droppin-dimes/posts",
+          fields: postSchema,
+      },
+    ],
+  },
+});
