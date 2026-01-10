be7bda3d5e6014c949d1902c10655fdc1a86f54b
John Mathena <bobtart41@gmail.com>
2026-01-09 20:23:45 -0500

    create web components for header and footer, update home page

---
+++ src/static/js/footer.js
@@ -0,0 +1,28 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="website-footer">
+          Johnny Dot Com © 2022 by John Mathena is licensed under
+          <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
+          <img
+            src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+            style="max-width: 1em; max-height: 1em; margin-left: 0.2em"
+          />
+          <img
+            src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+            style="max-width: 1em; max-height: 1em; margin-left: 0.2em"
+          />
+          <img
+            src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"
+            style="max-width: 1em; max-height: 1em; margin-left: 0.2em"
+          />
+        <
+    `;
+  }
+}
+
+customElements.define('custom-footer', Footer);
