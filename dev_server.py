8d834a5d14cd3d8d75f8353f681b38fe70077b3c
John Mathena <bobtart41@gmail.com>
2026-05-25 14:23:04 -0400

    fix: format dev server file

--- dev_server.py
+++ dev_server.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     server = Server()
 
-    server.watch("src/", shell('make build'))
+    server.watch("src/", shell("make build"))
 
     server.serve(port=8000, liveport=8001, root="public")
-
